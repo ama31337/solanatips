df8a2d06a83a302fff8e7a3b3384f748010ddba1
ama31337 <ama31337@protonmail.com>
2020-10-25 21:01:46 -0700

    update scripts updated

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,11 +4,12 @@ localversion=$(/home/$USER/.local/share/solana/install/active_release/bin/solana
 gitversion=$(curl --silent "https://api.github.com/repos/solana-labs/solana/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | sed 's/v//')
 diff <(echo "$localversion") <(echo "$gitversion")
 
-if [ $? -ne 0 ];
-        then
-                echo "need to update, localversion is $localversion, new release is $gitversion"
-		echo "start"
-
+if [ $? -ne 0 ]; then
+	echo "need to update, localversion is $localversion, new release is $gitversion"
+	echo -n "want to update (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then
+		echo "start update"
 		curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v${gitversion}/install/solana-install-init.sh | sh -s - v${gitversion}
 		solana-install update
 		sudo systemctl stop solana-tds
@@ -18,7 +19,11 @@ if [ $? -ne 0 ];
 		echo "current node version:"
 		echo "$localversion"
 	else
-		echo "localversion is $localversion, latest release is $gitversion"
-		echo "node is up to date"
+		echo "exit"
+	fi
+else
+	echo "localversion is $localversion, latest release is $gitversion"
+	echo "node is up to date"
+
 
 fi
