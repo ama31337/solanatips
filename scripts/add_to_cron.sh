94cd1f53cc6af6db52113b477c8179e0984f3b9f
ama31337 <ama31337@protonmail.com>
2021-02-11 13:53:50 +0000

    commit

--- scripts/add_to_cron.sh
+++ scripts/add_to_cron.sh
@@ -7,8 +7,9 @@ crontab -l |
     { cat; 
         echo "" ;
         echo "#solana cronjobs on $HOSTNAME" ;
-        echo "*/5 * * * *  $USER    cd $SCRIPT_DIR && ./check_sync.sh >> $HOME/logs/check_sync.log "; 
-        echo "*/5 * * * *  $USER    cd $SCRIPT_DIR && ./is_delinquent.sh >> $HOME/logs/is_delinquent.log ";
-        echo "0 */4 * * *  $USER    cd $SCRIPT_DIR && ./check_skip.sh >> $HOME/logs/check_skip.log ";
+        echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$HOME/.local/share/solana/install/active_release/bin
+        echo "*/5 * * * *    cd $SCRIPT_DIR && ./check_sync.sh >> $HOME/logs/check_sync.log "; 
+        echo "*/5 * * * *    cd $SCRIPT_DIR && ./is_delinquent.sh >> $HOME/logs/is_delinquent.log ";
+        echo "0 */4 * * *    cd $SCRIPT_DIR && ./check_skip.sh >> $HOME/logs/check_skip.log ";
     } | crontab -
 
