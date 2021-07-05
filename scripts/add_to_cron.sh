5f13d49b3fd4360cc16e16880ed9228c8492669e
ama31337 <ama31337@protonmail.com>
2021-07-05 20:35:42 +0700

    commit

--- scripts/add_to_cron.sh
+++ scripts/add_to_cron.sh
@@ -4,12 +4,13 @@ SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 mkdir -p $HOME/logs
 
 crontab -l | 
-    { cat; 
+    { 
+        #cat; 
         echo "" ;
         echo "#solana cronjobs on $HOSTNAME" ;
-        echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$HOME/.local/share/solana/install/active_release/bin
-        echo "*/5 * * * *    cd $SCRIPT_DIR && ./check_sync.sh >> $HOME/logs/check_sync.log "; 
-        echo "*/5 * * * *    cd $SCRIPT_DIR && ./is_delinquent.sh >> $HOME/logs/is_delinquent.log ";
-        echo "0 */4 * * *    cd $SCRIPT_DIR && ./check_skip.sh >> $HOME/logs/check_skip.log ";
+        echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$HOME/.local/share/solana/install/active_release/bin"
+        echo "#*/5 * * * *    cd $SCRIPT_DIR && ./check_sync.sh >> $HOME/logs/check_sync.log "; 
+        echo "0 */2 * * *    cd $SCRIPT_DIR && ./is_delinquent.sh >> $HOME/logs/is_delinquent.log ";
+        echo "0 0 * * *    cd $SCRIPT_DIR && ./check_skip.sh >> $HOME/logs/check_skip.log ";
     } | crontab -
 
