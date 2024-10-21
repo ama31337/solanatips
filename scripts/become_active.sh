6d61e19e328b20e2e720131f58e24d65e2f6a51e
ama31337 <ama31337@protonmail.com>
2024-10-21 13:45:10 +0530

    commit

--- scripts/become_active.sh
+++ scripts/become_active.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 BIN_PATH="$HOME/.local/share/solana/install/active_release/bin"
 
-${BIN_PATH}/solana-validator -l ${HOME}/validator-ledger set-identity --require-tower ${HOME}/keys/validator-keypair.json
+${BIN_PATH}/solana-validator -l ${LEDGER_PATH} set-identity --require-tower ${HOME}/keys/validator-keypair.json
 
 ln -sf ${HOME}/keys/validator-keypair.json ${HOME}/keys/identity.json
--- scripts/become_inactive.sh
+++ scripts/become_inactive.sh
@@ -2,19 +2,30 @@
 
 SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 BIN_PATH="$HOME/.local/share/solana/install/active_release/bin"
-DESTINATION="sol@${1}:/home/sol/validator-ledger/"
+
+# Set the local_test variable based on the value of TEST in env.sh
+LOCAL_LEDGER=${LEDGER_PATH}
+
+# Use ssh to source the env.sh file on the remote machine and set the remote_test variable
+REMOTE_LEDGER=${REMOTE_LEDGER_PATH}
+
+# Perform some actions using the local_test and remote_test variables
+echo "Local ledger path: ${LOCAL_LEDGER}"
+echo "Remote ledger path: ${REMOTE_LEDGER}"
+
+DESTINATION="sol@${1}:${REMOTE_LEDGER}"
 
 # check connection
 server=sol@${1}      # server IP
-port=555                 # ssh port
+port=58228                 # port
 connect_timeout=5       # Connection timeout
 ssh -q -o BatchMode=yes  -o StrictHostKeyChecking=no -o ConnectTimeout=$connect_timeout $server -p $port 'exit 0'
 if [ $? == 0 ];then
    echo "start script"
-   ${BIN_PATH}/solana-validator -l ${HOME}/validator-ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
-   ${BIN_PATH}/solana-validator -l ${HOME}/validator-ledger set-identity ${HOME}/keys/unstaked-identity.json
+   ${BIN_PATH}/solana-validator -l ${LOCAL_LEDGER} wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
+   ${BIN_PATH}/solana-validator -l ${LOCAL_LEDGER} set-identity ${HOME}/keys/unstaked-identity.json
    ln -sf ${HOME}/keys/unstaked-identity.json ${HOME}/keys/identity.json
-   scp -r -P $port ${HOME}/validator-ledger/tower-1_9-$(${BIN_PATH}/solana-keygen pubkey ${HOME}/keys/validator-keypair.json).bin ${DESTINATION}
+   rsync -avz -e "ssh -p $port" ${LOCAL_LEDGER}/tower-1_9-$(${BIN_PATH}/solana-keygen pubkey ${HOME}/keys/validator-keypair.json).bin ${DESTINATION}
    ssh sol@${1} -p $port 'bash -s' < ${SCRIPT_DIR}/become_active.sh
 else
    echo "SSH connection to $server over port $port is not possible, stop"
